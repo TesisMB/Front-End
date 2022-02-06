f17caae0a4ccb855d7c40e547f2ff01de70208a6
Player003 <matias_roldan89@hotmail.com>
2022-02-06 03:25:55 -0300

    v.3

--- src/app/resources/cart/cart.component.ts
+++ src/app/resources/cart/cart.component.ts
@@ -1,7 +1,9 @@
+import { AlertService } from './../../services/_alert.service/alert.service';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MaterialsService } from '../materials/materials.service';
 import { Request } from 'src/app/models';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 interface EmergenciesInput {
@@ -59,14 +61,17 @@ export class CartComponent implements OnInit, OnDestroy {
 
   
   request: Request = null;
-  error:any= "";
+  error:any;
   handle: any;
   form: FormGroup;
+  loading: boolean = false;
 
 
   constructor( 
     private requestService: MaterialsService,
     private formBuilder: FormBuilder,
+    private alertService: AlertService,
+    private _snackBar: MatSnackBar
 ) {
   this.getRequest();
 
@@ -91,10 +96,13 @@ export class CartComponent implements OnInit, OnDestroy {
 
   get f() {return this.form.controls;}
   get isRequest(){ return this.request ? true : false;}
-  get getStyle() { return this.request.request.length >= 2 ? '230px' : 'fit-content';}
+  get getStyle() { return this.request.request.length >= 2 ? '230px' : 'max-content';}
 
 
   submitCart(){
+    if(this.form.valid){
+    this.isLoading();
+
     const f = {
       Reason: this.form.get('Reason').value,
       FK_EmergencyDisasterID : 2,
@@ -120,27 +128,75 @@ export class CartComponent implements OnInit, OnDestroy {
 
     });
     console.log('Datos pre-post: ', f);
-    this.requestService.postRequest(f)
-   .subscribe(
-     data => this.clearCart(),
-     error => console.log('Error del post: ', error)
-   );
+    this.postCart(f);
   }
+}
+
   getRequest(){
+    this.isLoading();
+    
     this.handle = this.requestService._request
     .subscribe(
-      items => this.request = items,
-      error => this.error = error
-    );
-  }
+      items => {
+        this.request = items;
+        this.error = undefined;
+        this.loading = false;
+
+      },
+      error => {
+        this.error = error;
+       // this.isLoading();
+       // this.alertService.error('Ha ocurrido un error :(, intente mas tarde', { autoClose: true });
+          
+      });
+      }
+
   deleteFromCart(index:number){
   this.requestService.deleteFromCart(index);
-    
+  }
+
+  isLoading(){
+  this.loading =!this.loading;
+  console.log('loading: ',this.loading);
+  return this.loading;
   }
     clearCart(){
     this.requestService.clearCartRequest();
   }
+ 
+  postCart(f){
+    this.requestService.postRequest(f)
+   .subscribe(
+     data => {
 
+     // this.alertService.success('Solicitud enviada correctamente :)', {autoClose: true });
+      this._snackBar.open('Solicitud enviada correctamente :)','Cerrar', {
+        horizontalPosition:'center',
+        verticalPosition: 'top',
+        duration: 2000,
+      });
+      this.isLoading();
+      this.clearCart();
+      },
+     error =>{ 
+    this.alertService.error('Ha ocurrido un error', { autoClose: true });
+    console.log('Error del post: ', error.message);
+     this.isLoading();
+  } );
+  }
+  getErrorMessage(value) {
+    if(value <= 0)  {
+      return 'La cantidad minima para solicitar es 1.';
+    }  else {
+      return 'La cantidad maxima de stock disponible es';
+    } 
+   
+  }
+  getMax(i){
+  const max =  this.request.request[i].quantity + this.request.request[i].resource.quantity;
+  
+    return  max;
+  }
   ngOnDestroy(): void {
     this.handle.unsubscribe();
   }   
